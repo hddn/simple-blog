9a99e3c3c47da8e2f1676df7a6f1d636d5724476
hddn <hddnsmile@gmail.com>
2016-07-24 16:05:38 +0300

    add views

--- blog.py
+++ blog.py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+from datetime import datetime
 from flask import (Flask, request, sessions, g, redirect, 
                    url_for, abort, render_template, flash)
 
@@ -33,3 +34,61 @@ def get_db():
 def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
+
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+
+@app.cli.command('initdb')
+def initdb_command():
+    init_db()
+    print 'Initialized the database.'
+
+
+@app.route('/')
+def show_posts():
+    db = get_db()
+    cur = db.execute('select title, content, created from posts order by id desc')
+    posts = cur.fetchall()
+    return render_template('index.html', post=posts)
+
+
+@app.route('/add', methods=['POST'])
+def add_post():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db = execute('insert into posts (title, content, created) values (?, ?, ?)',
+                 [request.form['title'], request.form['content'], datetime.now()])
+    db.commit()
+    flash('New post was successfully posted')
+    return redirect(url_for('show_posts'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_posts'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_posts'))
+
+if __name__ == '__main__':
+    app.run()
